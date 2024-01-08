f3b4804c7343146d4f16c5bdad16eab75726375f
gufronnakaaw <gufronnakaaw@gmail.com>
2024-01-08 14:17:27 +0700

    install: material-tailwind, next-auth, react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +9,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        figtree: 'Figtree',
+        poppins: 'Poppins',
       },
     },
   },
   plugins: [],
-}
+});
