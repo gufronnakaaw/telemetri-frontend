3188d2763b7059d853f4ddf1c366fe3e4f489376
gufronnakaaw <gufronnakaaw@gmail.com>
2024-01-08 14:18:06 +0700

    create: middleware

---
+++ middleware.js
@@ -0,0 +1,24 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export default async function middleware(req) {
+  if (req.nextUrl.pathname == '/') {
+    const token = await getToken({ req, secret: 'telemetrikey' });
+
+    if (!token) {
+      return NextResponse.redirect(new URL('/auth/login', req.url));
+    }
+
+    return NextResponse.next();
+  }
+
+  if (req.nextUrl.pathname.startsWith('/auth')) {
+    const token = await getToken({ req, secret: 'telemetrikey' });
+
+    if (token) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    return NextResponse.next();
+  }
+}
