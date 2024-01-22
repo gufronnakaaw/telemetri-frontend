295f241bdb5c0f3784310255d1ce57f4ac32a4a8
gufronnakaaw <gufronnakaaw@gmail.com>
2024-01-22 15:57:51 +0700

    create: window event

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,33 @@
 import '@/styles/globals.css';
 
 import { ThemeProvider } from '@material-tailwind/react';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, getSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import NextNProgress from 'nextjs-progressbar';
-import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import Toast, { Toaster } from 'react-hot-toast';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    window.onfocus = async () => {
+      const session = await getSession();
+
+      if (router.asPath !== '/auth/login') {
+        if (!session) {
+          Toast.error('session expired', {
+            position: 'top-right',
+            duration: 1000,
+          });
+          signOut();
+        }
+      }
+    };
+  }, [router]);
   return (
     <SessionProvider session={session} refetchOnWindowFocus={false}>
       <ThemeProvider>
